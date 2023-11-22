b8b26d56d1fb56cb3b215dc7a86dbdc5e20595b9
Mehedi Hassan <mehedihassan2046@gmail.com>
2023-11-23 00:10:50 +0600

    complete custom slider

--- sliderjs.js
+++ sliderjs.js
@@ -1,3 +1,23 @@
+var count = 1;
+function slider() {
+    count++;
+    let currentElement = document.querySelector(".active")
+    let nextElement = currentElement.nextElementSibling;
+    let lastchild = document.querySelectorAll(".slide")
 
+    currentElement.classList.remove("active");
+
+    if (count > lastchild.length) {
+        lastchild[0].classList.add("active")
+        count = 1;
+    } else {
+
+        nextElement.classList.add("active")
+
+    }
+
+}
+
+setInterval(slider, 2000)
 
 
